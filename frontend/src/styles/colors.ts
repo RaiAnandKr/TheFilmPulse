5568239c0533f38c80a57de3a971251bac3f53ed
Souvik Saha <biswajitsahaicst@gmail.com>
2024-05-26 02:53:25 +0530

    Overriding primary color in tailwind config

---
+++ frontend/src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  transparent: "transparent",
+  current: "currentColor",
+  white: "#ffffff",
+  "teal-500": "#14b8a6",
+  "lime-500": "#84cc16",
+};
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,5 +1,6 @@
 import { nextui } from "@nextui-org/react";
 import { type Config } from "tailwindcss";
+import { colors } from "./src/styles/colors";
 
 export default {
   content: [
@@ -10,5 +11,19 @@ export default {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              DEFAULT: colors["teal-500"],
+              foreground: "#000000",
+            },
+            focus: colors["teal-500"],
+          },
+        },
+      },
+    }),
+  ],
 } satisfies Config;
