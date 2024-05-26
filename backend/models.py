ce3fc68ef1278561a6fc28d7208c9dc494eb9941
Anand Kumar <anandkumar@Anands-MacBook-Pro-2.local>
2024-05-26 12:19:13 +0530

    Add 'Opinion' model

--- backend/models.py
+++ backend/models.py
@@ -19,3 +19,27 @@ class User(db.Model):
     email: Mapped[str] = mapped_column(String(20), unique=True, nullable=True)
     name: Mapped[str] = mapped_column(String(20), nullable=True)
     is_banned: Mapped[bool] = mapped_column(default=False)
+
+@dataclass
+class Opinion(db.Model):
+    id: int
+    film_id: int
+    text: str
+    icon_url: str
+    user_count: int
+    yes_count: int
+    no_count: int
+    yes_coins: int
+    no_coins: int
+    author_id: int
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    film_id: Mapped[int] = mapped_cloumn(Integer, ForeignKey('film.id'))
+    text: Mapped[str] = mapped_column(String(200), unique=True, nullable=False)
+    icon_url: Mapped[str] = mapped_column(String(500))
+    user_count: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    yes_count: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    no_count: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    yes_coins: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    no_coins: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
+    author_id: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
