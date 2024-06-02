775f5399e2a40b3ac2c501f3b59b6ec4e999b7a3
j3rwin <jerwinn7@gmail.com>
2024-06-02 15:12:09 +0100

    Add OTPLess SDK

--- backend/auth_provider_config.py
+++ backend/auth_provider_config.py
@@ -1,42 +1,45 @@
 from abc import ABC, abstractmethod
 import requests
+import os
+import OTPLessAuthSDK
 
 
 class AuthProvider(ABC):
+    # TODO: Define return types (include error message)
     @abstractmethod
     def verify_token(self, **kwargs):
         pass
 
 
 class OTPLess(AuthProvider):
-    def __init__(self, **kwargs):
-        self.api_url = "https://auth.otpless.app/auth/otp/v1/verify"
-        self.app_id = kwargs.get('app_id')
-        self.client_id = kwargs.get('client_id')
-        self.client_secret = kwargs.get('client_secret')
+    def __init__(self):
+        # self.api_url = "https://auth.otpless.app/auth/otp/v1/verify"
+        # self.app_id = kwargs.get('app_id')
+        self.client_id = os.environ.get('OTPLESS_CLIENT_ID')
+        self.client_secret = os.environ.get('OTPLESS_CLIENT_SECRET')
 
     def verify_token(self, **kwargs):
-        headers = {
-            'clientId': self.client_id,
-            'clientSecret': self.client_secret
-        }
-        data = {
-            'orderId': kwargs.get('uid'),
-            'otp': kwargs.get('otp'),
-            'phoneNumber': kwargs.get('phone')
-        }
-        response = requests.post(self.api_url, json=data, headers=headers)
-
-        if response.status_code == 200:
-            return response.json()
-        else:
-            response.raise_for_status()
+        order_id = kwargs.get('uid')
+        otp = kwargs.get('otp')
+        phone_number = kwargs.get('phone_number')
+        print(order_id)
+        # TODO: exception handling
+        # This not a typo lol
+        result = OTPLessAuthSDK.OTP.veriy_otp(
+            orderId=order_id,
+            otp=otp,
+            phoneNumber=phone_number,
+            email=None,
+            client_id=self.client_id,
+            client_secret=self.client_secret)
+        # sample result={'isOTPVerified': False, 'reason': 'Incorrect OTP!'}
+        return result['isOTPVerified']
 
 
 class AuthProviderFactory:
     @staticmethod
-    def get_auth_provider(provider_name, **kwargs):
+    def get_auth_provider(provider_name):
         if provider_name == 'otpless':
-            return OTPLess(**kwargs)
+            return OTPLess()
         else:
             raise ValueError(f"Unsupported provider: {provider_name}")
