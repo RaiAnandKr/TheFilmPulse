64997459de7b7640b88068569b62506033aabe5f
Souvik Saha <biswajitsahaicst@gmail.com>
2024-06-04 18:07:35 +0530

    reverting redirect mechanism to use redirect api for fixing back button issue

--- frontend/src/app/pulse/page.tsx
+++ frontend/src/app/pulse/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 const Pulse = () => {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/pulse/opinions");
-  }, []);
-
-  return <></>;
+  redirect("pulse/opinions");
 };
 
 export default Pulse;
