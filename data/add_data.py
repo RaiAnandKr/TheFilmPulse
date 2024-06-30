033995906ddd2a9ba239ebc7998773142d9262cd
Anand Kumar <anandkumar@Anands-MacBook-Pro-2.local>
2024-06-30 23:54:37 +0530

    Add script to add data and also add data

--- data/add_data.py
+++ data/add_data.py
@@ -1,17 +1,58 @@
-import yaml
+import os
 import requests
+import yaml
 
-url = "http://yourapiurl.com/films"
+BASE_URL = os.environ.get("BACKEND_URL", "http://yourapiurl.com")
 
-# Load the YAML file
-with open('data.yaml', 'r') as file:
-    data = yaml.safe_load(file)
+def read_yaml():
+    with open('data.yaml', 'r') as file:
+        return yaml.safe_load(file)
 
-for film in data['films']:
-    print("films")
+def get_film_id(film_title):
+    response = requests.get(f"{BASE_URL}/films?film_title={film_title}")
+    response.raise_for_status()
+    films = response.json()
+    if films:
+        return films[0]["id"]
+    return None
 
-for opinion in data['opinions']:
-    print("opinions")
+def post_film(film):
+    response = requests.post(BASE_URL+"/films", json=film)
+    status_code, response = response.status_code, response.json()
+    print(f'Posted film: {film["title"]}, Status code: {status_code}, Response: {response}')
 
-for prediction in data['predictions']:
-    print("predictions")
+def post_opinion(opinion):
+    film_id = get_film_id(opinion["film_title"])
+    if not film_id:
+        return
+    opinion["film_id"] = film_id
+    del opinion["film_title"]
+
+    response = requests.post(BASE_URL+"/opinions", json=opinion)
+    status_code, response = response.status_code, response.json()
+    print(f'Posted opinion: {opinion["text"]}, Status code: {status_code}, Response: {response}')
+
+def post_prediction(prediction):
+    film_id = get_film_id(prediction["film_title"])
+    if not film_id:
+        return
+    prediction["film_id"] = film_id
+    del prediction["film_title"]
+    response = requests.post(BASE_URL+"/predictions", json=prediction)
+    status_code, response = response.status_code, response.json()
+    print(f'Posted prediction: {prediction["text"]}, Status code: {status_code}, Response: {response}')
+
+def main():
+    data = read_yaml()
+
+    for film in data['films']:
+        post_film(film)
+
+    for opinion in data['opinions']:
+        post_opinion(opinion)
+
+    for prediction in data['predictions']:
+        post_prediction(prediction)
+
+if __name__ == "__main__":
+    main()
