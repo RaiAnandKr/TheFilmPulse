b64a5c08182e5fb0680afa68900ac2313af16464
Souvik Saha <biswajitsahaicst@gmail.com>
2024-07-06 14:04:50 +0530

    Populating user context on app load

--- frontend/src/data/hooks/useLoadUserData.ts
+++ frontend/src/data/hooks/useLoadUserData.ts
@@ -1,13 +1,37 @@
 import { useMainStore } from "../contexts/store-context";
 import { useLoadData } from "./useLoadData";
-import { getUserCoins } from "~/service/apiUtils";
+import { getUser } from "~/service/apiUtils";
 import type { UserSlice } from "../store/user-slice";
+import { CoinType } from "~/schema/CoinType";
 
 export const useLoadUserData = <T>(selector: (state: UserSlice) => T): T => {
   const userInfo = useMainStore(selector);
-  const setUserCoins = useMainStore((state) => state.setUserCoins);
+  const setUserState = useMainStore((state) => state.setUser);
 
-  useLoadData("getUserCoins", getUserCoins, setUserCoins);
+  useLoadData("getUser", getUser, (user) => {
+    if (!user) {
+      return;
+    }
+
+    setUserState({
+      userId: user.id?.toString(),
+      phone: user.phoneNumber,
+      handle: user.userhandle,
+      isLoggedIn: true,
+      userCoins: [
+        {
+          type: CoinType.Earned,
+          coins: user.earnedCoins,
+          isRedeemable: true,
+        },
+        {
+          type: CoinType.Bonus,
+          coins: user.bonusCoins,
+          isRedeemable: false,
+        },
+      ],
+    });
+  });
 
   return userInfo;
 };
--- frontend/src/schema/User.ts
+++ frontend/src/schema/User.ts
@@ -1,11 +1,28 @@
-import type { Coin } from './Coin';
+import type { Coin } from "./Coin";
 
 export interface User {
+  id: number;
+  userhandle: string;
+  phoneNumber: string;
+  bonusCoins: number;
+  earnedCoins: number;
+  isBanned?: boolean;
+  email?: string;
+  state?: string;
+
+  // This is not some coin which is owned by the user but more
+  // of a custom field and hence keeping it separate.
+  maxOpinionCoins?: number;
+}
+
+export interface UserResponse {
+  bonus_coins: number;
+  earned_coins: number;
+  id: number;
+  is_banned: boolean;
+  phone_number: string;
   username: string;
   email?: string;
   state?: string;
-  coins: Coin[];
-  // This is not some coin which is owned by the user but more
-  // of a custom field and hence keeping it separate.
-  maxOpinionCoins: number;
+  max_opinion_coins?: number;
 }
