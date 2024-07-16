a9ce954fd6a61f604c5905e71503fe8d22565bd1
Anand Kumar <anandkumar@Anands-MacBook-Pro-2.local>
2024-07-16 19:28:34 +0530

    Integrate Meta pixel

--- frontend/src/app/metrics/index.tsx
+++ frontend/src/app/metrics/index.tsx
@@ -1,10 +1,12 @@
 import GoogleAnalytics from "./GoogleAnalytics";
 import MicrosoftClarity from "./MicrosoftClarity";
+import MetaPixel from "./Meta.tsx";
 
 const Metrics = () => (
     <>
         <GoogleAnalytics />
         <MicrosoftClarity />
+        <MetaPixel />
     </>
 );
 
