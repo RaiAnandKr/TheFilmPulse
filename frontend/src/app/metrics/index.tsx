ef8d0b174f4b4a1fdec845eef0469f2280613593
Anand Kumar <anandkumar@Anands-MacBook-Pro-2.local>
2024-07-17 02:06:55 +0530

    Render the metrics component once per session

--- frontend/src/app/metrics/index.tsx
+++ frontend/src/app/metrics/index.tsx
@@ -1,13 +1,27 @@
-import GoogleAnalytics from "./GoogleAnalytics";
-import MicrosoftClarity from "./MicrosoftClarity";
-import MetaPixel from "./Meta";
+'use client';
 
-const Metrics = () => (
+import { useEffect, useState } from 'react';
+import GoogleAnalytics from './GoogleAnalytics';
+import MicrosoftClarity from './MicrosoftClarity';
+import MetaPixel from './MetaPixel';
+
+const Metrics = () => {
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    if (!initialized) {
+      setInitialized(true);
+    }
+  }, [initialized]);
+
+  return (
     <>
-        <GoogleAnalytics />
-        <MicrosoftClarity />
-        <MetaPixel />
+      {initialized && <GoogleAnalytics />}
+      {initialized && <MicrosoftClarity />}
+      {initialized && <MetaPixel />}
     </>
-);
+  );
+};
 
 export default Metrics;
+
