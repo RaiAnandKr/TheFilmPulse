1ba8a096ea6f222930d1a0be14c8e253474af781
Anand Kumar <anandkumar@Anands-MacBook-Pro-2.local>
2024-08-09 17:12:19 +0530

    Route to /contests when someone lands on film

--- frontend/src/app/film/[filmId]/page.tsx
+++ frontend/src/app/film/[filmId]/page.tsx
@@ -3,7 +3,7 @@
 import { redirect } from "next/navigation";
 
 const FilmPage = ({ params }: { params: { filmId: string } }) => {
-  redirect(`${params.filmId}/opinions`);
+  redirect(`${params.filmId}/contests`);
 };
 
 export default FilmPage;
